be3328f182608c576b86902c9dfb8d435d074dbe
Rui Dias <ruipecdias@gmail.com>
2023-11-27 16:11:28 +0000

    update users

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,3 +1,10 @@
 class Incident < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  has_many :affecting_pins
+
+  validates :location, presence: true
+  validates :category, presence: true, inclusion: { in: ['Accident', 'Construction', 'Structural'] }
+  validates :description, presence: true
+  validates :status, inclusion: { in: ['resolved', 'unresolved'] }
 end
