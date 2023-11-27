1c9df455eff50d9d4d3b9e5349068b029e216b2c
Sihao Xiao <sixiao@ucsd.edu>
2023-11-27 13:21:43 +0000

    added models and controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_122747) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_27_130356) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "incident_id", null: false
+    t.bigint "user_id", null: false
+    t.text "content"
+    t.string "img_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["incident_id"], name: "index_comments_on_incident_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "incidents", force: :cascade do |t|
+    t.string "location"
+    t.string "category"
+    t.string "img_url"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.integer "comments_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_incidents_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_122747) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "incidents"
+  add_foreign_key "comments", "users"
+  add_foreign_key "incidents", "users"
 end
