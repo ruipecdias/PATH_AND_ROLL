5fa3e58c65adbf4775c6925f0c7f33f611f95750
Sihao Xiao <sixiao@ucsd.edu>
2023-11-27 16:02:33 +0000

    added affecting pins controller

---
+++ app/controllers/affecting_pins_controller.rb
@@ -0,0 +1,2 @@
+class AffectingPinsController < ApplicationController
+end
---
+++ test/controllers/affecting_pins_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class AffectingPinsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
