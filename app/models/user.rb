50697bf558dcb2ba4a29eb5e03fcd7eb56407184
Rui Dias <ruipecdias@gmail.com>
2023-12-01 12:08:32 +0000

    added avatar to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :photo
+
   validates :first_name, presence: true
   validates :last_name, presence: true
 
