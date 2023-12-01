9c4fa0834fa27ce8a062395c9921e7d6d58ccf55
Sihao Xiao <sixiao@ucsd.edu>
2023-12-01 14:46:52 +0000

    added incident with real picture from antonella

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,23 +27,25 @@ locations = [
 # Safe image URLs for Cloudinary
 
 imgurls =[
-  "https://res.cloudinary.com/dnwea2j0w/image/upload/v1701366720/hzx0n0imr2wx0nbqvfvc.jpg",
-  "https://res.cloudinary.com/dnwea2j0w/image/upload/v1701366718/wj1oslsnfrv6rearwgfp.jpg",
+  "https://res.cloudinary.com/dnwea2j0w/image/upload/v1701441105/IMG_0260_oqg0ll.jpg",
+  "https://res.cloudinary.com/dnwea2j0w/image/upload/v1701441103/IMG_0290_npedl5.jpg",
   "https://res.cloudinary.com/dnwea2j0w/image/upload/v1701366717/tklbcdn3tzf9wc1omqwq.jpg",
   "https://res.cloudinary.com/dnwea2j0w/image/upload/v1701366716/al1f1zln67d88uvqnoh8.jpg",
   "https://res.cloudinary.com/dnwea2j0w/image/upload/v1701366715/tvyw5lplhymmdesixo4n.jpg",
   "https://res.cloudinary.com/dnwea2j0w/image/upload/v1701366713/cvtmufvmwf8zpbesoo2e.jpg",
   "https://res.cloudinary.com/dnwea2j0w/image/upload/v1701366711/wr2v5lzdfpfee7tuewpv.jpg",
   "https://res.cloudinary.com/dnwea2j0w/image/upload/v1701366710/iwaxudgt5xp7xjhehmbi.jpg",
-  "https://res.cloudinary.com/dnwea2j0w/image/upload/v1701366709/xwnf3shf7jp9fiqueyms.jpg",
-  "https://res.cloudinary.com/dnwea2j0w/image/upload/v1701364904/gqvmcdswtua5dytv5zne.jpg"
+  "https://res.cloudinary.com/dnwea2j0w/image/upload/v1701441106/IMG_0295_pvnj3o.jpg",
+  "https://res.cloudinary.com/dnwea2j0w/image/upload/v1701441123/IMG_0261_tcpa91.jpg"
 ]
 
 
 
 
 imgurl = "https://res.cloudinary.com/dnwea2j0w/image/upload/v1701366717/tklbcdn3tzf9wc1omqwq.jpg"
-additional_imgurl = "https://gibb.pt/wp-content/uploads/2017/09/IC17-%E2%80%93-CRIL-Sublan%C3%A7o-Buraca-Pontinha-%E2%80%93-Trabalhos-Complementares-%E2%80%93-Reposi%C3%A7%C3%A3o-do-Caneiro-da-Damaia-300x225.jpg"
+additional_imgurls = ["https://res.cloudinary.com/dnwea2j0w/image/upload/v1701441104/IMG_0294_qsdagt.jpg",
+"https://res.cloudinary.com/dnwea2j0w/image/upload/v1701441102/IMG_0289_pqwoav.jpg",
+"https://res.cloudinary.com/dnwea2j0w/image/upload/v1701365047/xlfu79agdpnnard3j88n.jpg"]
 
 # Create Incidents
 locations.each_with_index do |location, index|
@@ -70,8 +72,8 @@ locations.each_with_index do |location, index|
   incident.photos.attach(io: file, filename: "location_#{index}.jpg", content_type: 'image/jpg')
 
   # For the first incident, add an additional image
-  if index == 0
-    additional_file = URI.open(additional_imgurl)
+  if index < 3
+    additional_file = URI.open(additional_imgurls[index])
     incident.photos.attach(io: additional_file, filename: "additional_location_#{index}.jpg", content_type: 'image/jpg')
   end
 end
